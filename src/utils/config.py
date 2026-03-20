4912b46b9b92ac2ab24e6f4724856ccae1c24892
ThanhNhan <thanhnhan959604@gmail.com>
2026-03-20 14:07:30 +0700

    them cac file config http

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,1 +1,30 @@
 import os
+from pathlib import Path
+
+#lớp này chứa toàn bộ đường dẫn và cẩu hình của dự á
+class PiplineConfig:
+    
+    #lấy đường dẫn gốc
+    ROOT_DIR = Path(__file__).resolve().parent.parent.parent
+    
+    #các thư mục chính
+    DATA_DIR = ROOT_DIR/"data"
+    CACHE_DIR = ROOT_DIR/"cache"
+    AUDIO_DIR = DATA_DIR/"audio_previews"
+    PROCESSED_DIR = DATA_DIR/"processed"
+    
+    os.makedirs(DATA_DIR, exist_ok=True)
+    os.makedirs(CACHE_DIR, exist_ok=True)
+    os.makedirs(AUDIO_DIR, exist_ok=True)
+    os.makedirs(PROCESSED_DIR, exist_ok=True)
+    
+    
+    #đường dẫn các file đầu ra theo pipeline
+    
+    #bước 1: 
+    TRACK_IDS_FILE = CACHE_DIR/"track_ids.txt"
+    CRAWLED_ARTISTS_FILE = CACHE_DIR/"crawled_artists.txt"
+    
+    #từ khoá mầm ban đầu
+    SEED_KEYWORDS = ["Sơn Tùng M-TP", "Đen Vâu", "Hoàng Thùy Linh", "HIEUTHUHAI", "B Ray", "V-pop"]
+    TARGET_COUNT = 10000
---
+++ src/utils/http_client.py
@@ -0,0 +1,51 @@
+import os
+import time
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Spotify81Client:
+    
+    def __init__(self):
+        self.api_key = os.getenv("RAPIDAPI_KEY")
+        self.host = "spotify81.p.rapidapi.com"
+        
+        #kiểm tra env có tồn tại
+        if not self.api_key:
+            raise ValueError("[LOI]: Không tìm thấy RAPIDAPI_KEY")
+        
+        self.headers = {
+            "x-rapidapi-key": self.api_key,
+            "x-rapidapi-host": self.host,
+            "Content-Type": "application/json"
+        }
+    
+    #hàm gửi yêu cầu
+    def get(self, endpoint, params=None, max_retries=3):
+        
+        url = f"https://{self.host}{endpoint}"
+        
+        for attempt in range(max_retries):
+            try:
+                #gửi yêu cầu lên server
+                response = requests.get(url=url, headers=self.headers, params=params, timeout=15)
+                
+                if response.status_code == 200:
+                    return response.json()
+                
+                elif response.status_code == 429:
+                    print(f"[LOI] Server đang quá tải (lỗi 429). Nghỉ 15s... (Lần thử {attempt + 1}/{max_retries})")
+                    time.sleep(15)
+                    
+                #nếu sai key hoặc đạt limit của API
+                elif response.status_code == 403:
+                    print("[LOI] Lỗi API ({response.status_code}): {response.text}")
+                    break
+            
+            except requests.exceptions.RequestException as e:
+                print(f"[LOI] Lỗi mạng: {e}. Đang thử lại...")
+                time.sleep(5)
+                
+        return None
