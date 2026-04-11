68058928808d21145ef2824defe1a07301240bdc
ThanhNhan <thanhnhan959604@gmail.com>
2026-04-11 21:43:56 +0700

    chinh sua step 1 va config

--- src/utils/config.py
+++ src/utils/config.py
@@ -46,9 +46,38 @@ class PipelineConfig:
     # step07: train-ready dataset
     TRAIN_READY_FILE = DATA_DIR / "step7_train_ready.csv"
     
-    #từ khoá mầm ban đầu
-    SEED_KEYWORDS = ["Sơn Tùng M-TP", "Đen Vâu", "Hoàng Thùy Linh", "HIEUTHUHAI", "B Ray", "V-pop"]
-    TARGET_COUNT = 10000
+    # DANH SÁCH ~200 NGHỆ SĨ & TỪ KHÓA NHẠC VIỆT
+
+    SEED_KEYWORDS = [
+        "Sơn Tùng M-TP", "Đen Vâu", "Hoàng Thùy Linh", "HIEUTHUHAI", "B Ray", "Jack - J97",
+        "Phan Mạnh Quỳnh", "Bích Phương", "Tóc Tiên", "Min", "Erik", "Đức Phúc", "Hòa Minzy",
+        "Trúc Nhân", "Karik", "Binz", "JustaTee", "Rhymastic", "Suboi", "Wowy", "MCK", "tlinh",
+        "Obito", "Low G", "Vũ.", "Ngọt", "Chillies", "Cá Hồi Hoang", "Da LAB", "Thái Đinh",
+        "Lê Cát Trọng Lý", "Amee", "Suni Hạ Linh", "Phùng Khánh Linh", "Grey D", "MONO",
+        "Tăng Duy Tân", "Wren Evans", "Myra Trần", "Văn Mai Hương", "Trung Quân Idol",
+        "Noo Phước Thịnh", "Đông Nhi", "Ông Cao Thắng", "Bảo Anh", "Hương Tràm", "Chi Pu",
+        "Soobin Hoàng Sơn", "Cầm", "Phương Ly", "Thùy Chi", "Mỹ Tâm", "Hà Anh Tuấn", "Lệ Quyên",
+        "Tuấn Hưng", "Bằng Kiều", "Hồ Ngọc Hà", "Đàm Vĩnh Hưng", "Quang Dũng", "Thanh Thảo",
+        "Đan Trường", "Cẩm Ly", "Lam Trường", "Phương Thanh", "Thu Minh", "Ưng Hoàng Phúc",
+        "Phạm Quỳnh Anh", "Khổng Tú Quỳnh", "Ngô Kiến Huy", "Miu Lê", "Trịnh Thăng Bình",
+        "Lou Hoàng", "Only C", "Khắc Việt", "Vũ Duy Khánh", "Châu Khải Phong", "Hồ Quang Hiếu",
+        "Phan Đinh Tùng", "Quang Vinh", "Bảo Thy", "Đinh Hương", "Thảo Trang", "Uyên Linh",
+        "Quốc Thiên", "Hương Giang", "Phạm Hồng Phước", "Tiên Cookie", "Tiên Tiên", "Trang",
+        "Nhạc Trẻ", "V-Pop", "Nhạc Việt Nam", "Rap Việt", "Indie Việt", "Nhạc Lofi Việt",
+        "Acoustic Việt", "Remix Việt", "Vinahouse", "Bolero", "Nhạc Xưa", "Nhạc Trữ Tình",
+        "Đạt G", "Du Uyên", "Hoa Vinh", "Jack và K-ICM", "K-ICM", "Masew", "Pháo", "Xesi",
+        "Ricky Star", "Seachains", "Dế Choắt", "Lăng LD", "Yuno Bigboi", "G-Ducky", "Tage",
+        "Lil Wuyn", "B-Wine", "16 Typh", "Gonzo", "Thành Draw", "Orijinn", "RPT JasonDilla",
+        "Vsoul", "Robber", "TGSN", "Wxrdie", "Nger", "Right", "Mikelodic",
+        "HuyR", "Đình Dũng", "Thái Học", "Thương Võ", "Hương Ly", "Gia Huy Singer",
+        "Quân A.P", "Anh Tú", "LyLy", "Châu Đăng Khoa", "Orange", "Sofia", "Khói",
+        "Dick", "Huỳnh James", "Pjnboys", "Nguyễn Trần Trung Quân", "Denis Đặng",
+        "Thiều Bảo Trâm", "Thiều Bảo Trang", "Ali Hoàng Dương", "Juky San", "Thịnh Suy",
+        "Kiên", "Táo", "Đạt Maniac", "Namling", "G5R Squad", "Jombie", "Lục Huy",
+        "Cody", "Uni5", "Lip B", "MONSTAR", "OPlus", "Ngọt Band", "Bức Tường", "Microwave",
+        "Ngũ Cung", "Cát Tiên", "Lâm Chấn Khang", "Lâm Chấn Huy", "Châu Việt Cường",]
+
+    TARGET_COUNT = 100000
 
     # whitelist nghệ sĩ Việt
     KNOWN_VIET_ARTISTS = {
